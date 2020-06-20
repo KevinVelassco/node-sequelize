5f0578186698af2b209ac21edde155322cb36133
Kevin Andres Velasco <kevinvelassco@gmail.com>
2020-06-20 16:12:25 -0500

    are added validations to the fields of the category model

--- models/category.js
+++ models/category.js
@@ -3,21 +3,65 @@ module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate:{
+        notNull: {
+          msg: 'The title field is required'
+        },
+        notEmpty: {
+          msg: "The title field is required"
+        },
+        len: {
+          args: [5,20],
+          msg: "The title must have between 5 and 20 characters."
+        }
+      }
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'The description field is required'
+        },
+        notEmpty: {
+          msg: "The description field is required"
+        },
+        len: {
+          args: [20,undefined],
+          msg: "The description must have at least 20 characters"
+        }
+      }      
     },
     alias: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
+      validate:{
+        notNull: {
+          msg: 'The alias field is required'
+        },
+        notEmpty: {
+          msg: "The alias field is required"
+        },
+        len: {
+          args: [1,8],
+          msg: "The alias must have a maximum of 8 characters."
+        }
+      }
     },
     color: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
+      validate:{
+        notNull: {
+          msg: 'The color field is required'
+        },
+        notEmpty: {
+          msg: "The color field is required"
+        }
+      }      
     },
   }, {});
   Category.associate = function(models) {
