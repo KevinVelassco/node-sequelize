20e361a1b19368e5dcbd11cc42b2aa0b5bae63b2
Kevin Andres Velasco <kevinvelassco@gmail.com>
2020-06-20 11:10:42 -0500

    is performed operations crud of the task model

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,20 +1,45 @@
 const express = require("express");
-const Task = require("../models/task");
+const { Task } = require("../models");
+const paginate = require("../paginate/paginate");
+const { handlingRequests } = require("../middlewares/handling-requests");
+const requestDataAllowed = ["title", "description", "state"];
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    Task.findAll()
+    const { page } = req.query;
+    paginate(Task, page, {
+        order: [["id", "DESC"]]
+    })
     .then(tasks => res.json(tasks))
     .catch(err => res.json(err));
 });
 
-router.post("/", (req, res) => {
-    const {title, description, state} = req.body;
+router.get("/:id", (req, res) => {
+    Task.findByPk(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+});
 
-    Task.create({
-        title, 
-        description,
-        state
+router.post("/", handlingRequests(requestDataAllowed), (req, res) => {
+    Task.create(req.body)
+    .then(task => res.json(task))
+    .catch(err => res.json(err));
+});
+
+router.put("/:id", handlingRequests(requestDataAllowed), (req, res) => {
+    Task.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(task => res.json(task))
+    .catch(err => res.json(err));
+});
+
+router.delete("/:id", (req, res) => {
+    Task.destroy({
+        where: {
+            id: req.params.id
+        }
     }).then(task => res.json(task))
     .catch(err => res.json(err));
 });
