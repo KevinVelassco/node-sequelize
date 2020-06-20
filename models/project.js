69c0436dd1c773a75d57ce394f6ce6a74d2e16d0
Kevin Andres Velasco <kevinvelassco@gmail.com>
2020-06-20 16:50:03 -0500

    are added validations to the fields of the project model

--- models/project.js
+++ models/project.js
@@ -5,6 +5,9 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
+        notNull: {
+          msg: 'The title field is required'
+        },
         len: {
           args: [5, 20],
           msg: "The title must have between 5 and 20 characters."
@@ -13,7 +16,19 @@ module.exports = (sequelize, DataTypes) => {
     },
     description: {
       type: DataTypes.TEXT,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'The description field is required'
+        },
+        notEmpty: {
+          msg: "The description field is required"
+        },
+        len: {
+          args: [20,undefined],
+          msg: "The description must have at least 20 characters"
+        }
+      }
     },
     state: {
         type: DataTypes.BOOLEAN,
