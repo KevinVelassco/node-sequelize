4a2345583c9ddb2dca619a633056431016687a43
Kevin Andres Velasco <kevinvelassco@gmail.com>
2021-06-21 18:07:36 -0500

    adjustment in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-var cors = require('cors');
+const cors = require('cors');
 const morgan = require("morgan");
 const handlingErrors = require("./middlewares/handling-errors");
 const users = require("./routes/users");
